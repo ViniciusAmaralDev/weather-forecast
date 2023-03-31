b448a6f4c171ad184a58e33503edba35132db937
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-31 15:58:05 -0300

    feat: adicionado lib react-native-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,22 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+          alias: {
+            "@assets": "./src/application/assets",
+            "@components": "./src/application/components",
+            "@flows": "./src/application/flows",
+            "@hooks": "./src/application/hooks",
+            "@theme": "./src/application/theme",
+          },
+        },
+      ],
+    ],
   };
 };
