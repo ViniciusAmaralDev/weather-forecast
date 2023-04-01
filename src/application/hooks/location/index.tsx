6bbeb31f48ac01566ca7475b51c2f34256c5372e
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-03-31 21:58:29 -0300

    refactor: atualizado hook location

--- src/application/hooks/location/index.tsx
+++ src/application/hooks/location/index.tsx
@@ -1,3 +1,4 @@
+import * as ExpoLocation from "expo-location";
 import { Children } from "../../../@types/Children";
 import { Location, LocationContext } from "./types";
 import GeocodingService from "infrastructure/services/geocoding";
@@ -20,8 +21,23 @@ export const LocationProvider = ({ children }: Children) => {
     }
   };
 
+  const getLocation = async () => {
+    let { status } = await ExpoLocation.requestForegroundPermissionsAsync();
+    if (status !== "granted") return;
+
+    let {
+      coords: { latitude, longitude },
+    } = await ExpoLocation.getCurrentPositionAsync({});
+    return { latitude, longitude };
+  };
+
   useEffect(() => {
-    // convertAddressToCoordinates("São Luís, Maranhão");
+    (async () => {
+      const coords = await getLocation();
+      const { data } = await service.convertCoordinatestoAddress(coords);
+      delete data[0].local_names;
+      setLocation(data[0]);
+    })();
   }, []);
 
   return (
