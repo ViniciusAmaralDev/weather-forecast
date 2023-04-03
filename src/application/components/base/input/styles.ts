531bbb748561dabf8c7942751820965bcaafc73c
ViniciusAmaralDev <viniciusamaraldev@gmail.com>
2023-04-03 10:08:55 -0300

    style: corrigido estilo

--- src/application/components/base/input/styles.ts
+++ src/application/components/base/input/styles.ts
@@ -4,5 +4,5 @@ export const TextInput = styled.TextInput`
   flex: 1;
   font-family: ${({ theme }) => theme.fonts.REGULAR};
   font-size: 14px;
-  color: ${({ theme }) => theme.colors.BLACK};
+  color: ${({ theme }) => theme.colors.TEXT_PRIMARY};
 `;
